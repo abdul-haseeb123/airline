d5d647e3a460a60a8d58e0bb3c83c62ed4805893
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2022-04-09 19:49:46 +0500

    First Commit

---
+++ flights/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+
+import flights
+from .models import Flight, Passenger
+# Create your views here.
+def index(request):
+    return render(request, "flights/index.html",{
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html",{
+        "flight":flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flight=flight).all()
+    })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        # Getting the current flight where I have to add a passenger
+        flight = Flight.objects.get(pk=flight_id)
+        # Getting The Passenger id via the form method of POST
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flight.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id, ))) 
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from users import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
+]
